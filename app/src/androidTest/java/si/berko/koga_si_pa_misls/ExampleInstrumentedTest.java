83f2c79cddc2a756895d55491e02374ec5583ce6
Peter Berkopec <4epberko@DIJAK.ZAVOD>
2018-01-18 15:23:35 +0100

    Initial commit

---
+++ app/src/androidTest/java/si/berko/koga_si_pa_misls/ExampleInstrumentedTest.java
@@ -0,0 +1,26 @@
+package si.berko.koga_si_pa_misls;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("si.berko.koga_si_pa_misls", appContext.getPackageName());
+    }
+}
---
+++ app/src/main/java/si/berko/koga_si_pa_misls/MainActivity.java
@@ -0,0 +1,13 @@
+package si.berko.koga_si_pa_misls;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
