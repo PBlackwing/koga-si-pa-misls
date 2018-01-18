8d456518372ec5f57903920d3084d5bc2c2509ad
PBlackwing <35568420+PBlackwing@users.noreply.github.com>
2018-01-18 15:39:03 +0100

    Update MainActivity.java

--- app/src/main/java/si/berko/koga_si_pa_misls/MainActivity.java
+++ app/src/main/java/si/berko/koga_si_pa_misls/MainActivity.java
@@ -9,6 +9,11 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        wdfjbvkswhfjbveswbfbfkewbewbg
+    
+        
+        
+        
+        
+        
     }
 }
